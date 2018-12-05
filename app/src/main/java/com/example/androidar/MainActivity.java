6df9d51329c5967499580c45c8aa030b78f3b5e1
Garttox <garto25@gmail.com>
2018-12-05 20:31:34 +0100

    mainActivity logo

--- app/src/main/java/com/example/androidar/MainActivity.java
+++ app/src/main/java/com/example/androidar/MainActivity.java
@@ -6,13 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logo = (ImageView) findViewById(R.id.imageView);
+        logo.setImageResource(R.drawable.logored);
     }
 
     //Spuštění Map
